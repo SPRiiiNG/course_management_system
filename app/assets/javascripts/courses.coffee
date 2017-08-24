48b18771584e5d31a58bee518079c53ffae8c3e8
SPRiiiNG <palee.wasan@gmail.com>
2017-08-25 00:59:26 +0700

    edit course index and search bar

--- app/assets/javascripts/courses.coffee
+++ app/assets/javascripts/courses.coffee
@@ -1,3 +1,41 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready ->
+#   # JSON.parse('<%= @link.errors.messages.to_json.html_safe%>')
+#   # chart_data_row = '<%= "#{@chart_data_row.to_json}" %>'
+#
+#   show_date = JSON.parse($('#start_time').val()).split(" ")[0]
+#   if $('#start_time').val().split("/")[0] != $('#end_time').val().split("/")[0]
+#     show_date = show_date+" - "+JSON.parse($('#end_time').val()).split(" ")[0]
+#
+  currentdate = new Date
+  cur_date = (currentdate.getMonth()+1)+"/"+currentdate.getDate()+"/"+currentdate.getFullYear()
+
+  $('#datetimepicker_start').datetimepicker
+    format: 'DD/MM/YYYY HH:mm',
+    defaultDate: cur_date+ " 10:00"
+  $('#datetimepicker_end').datetimepicker
+    useCurrent: false,
+    format: 'DD/MM/YYYY HH:mm',
+    defaultDate: cur_date+ " 23:59"
+
+  $('#datetimepicker_start').on 'dp.change', (e) ->
+    $('#datetimepicker_end').data('DateTimePicker').minDate e.date
+    if $('#start_time').val() > $('#end_time').val()
+      $('#end_time').val($('#start_time').val())
+    setup_attr()
+    return
+
+  $('#datetimepicker_end').on 'dp.change', (e) ->
+    $('#datetimepicker_start').data('DateTimePicker').maxDate e.date
+    setup_attr()
+    return
+
+  setup_attr = () ->
+    course_name = $('#course_name').val()
+    $('#btn-search').attr("href","/courses?course_name=" + course_name.toString())
+
+  setup_attr()
+  $('#course_name').on 'change', () ->
+    setup_attr()
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,14 +1,10 @@
 class CoursesController < ApplicationController
+  before_action :get_nav_data
+  before_action :get_courses
 
   def index
-    # courses = Courses.where(
-    #   :start_time.gte => @start_time_query,
-    #   :start_time.lte => @end_time_query
-    # )
-    courses = Course.where(
-      start_time: @start_time_query,
-      start_time: @end_time_query
-    )
+    @course_fields =  ["name", "user", "start_time", "end_time", "capacity"]
+    @courses = sort_by_params(@courses) if sort_params.present?
   end
 
   private
@@ -20,13 +16,40 @@ class CoursesController < ApplicationController
   end
 
   def get_nav_data
-    @start_time_query = Time.zone.parse(query_params[:start_time]) rescue today_with(8,0,0)
-    @end_time_query = Time.zone.parse(query_params[:end_time]) rescue today_with(20,0,0)
-    @start_time = @start_time_query.strftime("%d/%m/%Y %H:%M")
-    @end_time = @end_time_query.strftime("%d/%m/%Y %H:%M")
+    @start_time_query = Time.zone.parse(query_params[:start_time]) rescue nil
+    @end_time_query = Time.zone.parse(query_params[:end_time]) rescue nil
+    # @start_time = @start_time_query.strftime("%d/%m/%Y %H:%M")
+    # @end_time = @end_time_query.strftime("%d/%m/%Y %H:%M")
   end
 
-  def date_params
-    params.permit(:start_time, :end_time)
+  def get_courses
+    find_params = {}
+    if @start_time_query.present?
+      @start_time = @start_time_query.strftime("%d/%m/%Y %H:%M")
+      find_params[:start_time.gte] = @start_time_query
+    end
+    if @end_time_query.present?
+      @end_time = @end_time_query.strftime("%d/%m/%Y %H:%M")
+      find_params[:end_time.lte] = @end_time_query
+    end
+    find_params[:name] = /(\A#{query_params[:course_name]}|\s#{query_params[:course_name]})/i  if query_params[:course_name].present?
+    # asdsad
+    @courses = Course.where(find_params)
+  end
+
+  def sort_params
+    params.require(:sort) rescue nil
+  end
+
+  def query_params
+    params.permit(:course_name, :start_time, :end_time)
+  end
+
+  def sort_by_params(sort_data)
+    sort = sort_params.split(':')
+    @sort = {}
+    @sort['field'] = sort[0]
+    @sort['direction'] = sort[1]
+    sort_data.order_by([[@sort['field'], @sort['direction']]])
   end
 end
